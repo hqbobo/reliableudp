f70611e245a0db88b83640f7ff7f60ac4a1cd00e
hqbobo <29414@qq.com>
2019-06-12 22:56:39 +0800

    up

--- client.go
+++ client.go
@@ -4,18 +4,54 @@ import (
 	"fmt"
 	"net"
 )
+
 type Client struct {
-	port int
-	ip string
-	conn net.Conn
+	port    int
+	ip      string
+	conn    net.Conn
+	svrconn *net.UDPConn
+	exit    bool
 }
 
-func (this *Client) init (){
+func (this *Client) recv() {
+	for {
+		if this.exit {
+			break
+		}
+		buf := make([]byte, 65535)
+
+		n, raddr, err := this.svrconn.ReadFromUDP(buf[0:])
+		fmt.Print("client:")
+		if err != nil {
+			fmt.Println("from ReadFromUDP:", err)
+		} else {
+			fmt.Println(raddr.String(), "-recv:", n)
+		}
+
+	}
+
+}
+
+func (this *Client) init() {
+	var addr  *net.UDPAddr
+
 	conn, err := net.Dial("udp", fmt.Sprintf("%s:%d", this.ip, this.port))
 	if err != nil {
 		fmt.Println(err)
 	}
 	this.conn = conn
+
+	if addr, err = net.ResolveUDPAddr("udp", conn.LocalAddr().String()); err != nil {
+		fmt.Println(err)
+	}
+
+	this.svrconn, err = net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println(err)
+	}
+	this.exit = false
+	go this.recv()
+
 }
 
 func (this *Client) Send(msg []byte) (int, error) {
